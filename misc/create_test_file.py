46a1df56d8f22eb14df224ed7a433e273f62a2ba
Bjørn Tore Håvie <itsbth@itsbth.com>
2023-06-18 22:48:22 +0200

    test: add vhs, fixed entry in test data

--- misc/create_test_file.py
+++ misc/create_test_file.py
@@ -1,8 +1,30 @@
 import gzip
 
 IN_FILE = "JMdict_e.gz"
-OUT_FILE = "JMdict_e_test.gz"
+OUT_FILE = "testdata/JMdict_e_test.gz"
 NUM_ENTRIES = 100
+
+# known entry for testing
+AKASABI_ENTRY = """
+<entry>
+<ent_seq>1829380</ent_seq>
+<k_ele>
+<keb>赤錆</keb>
+</k_ele>
+<k_ele>
+<keb>赤さび</keb>
+</k_ele>
+<r_ele>
+<reb>あかさび</reb>
+</r_ele>
+<sense>
+<pos>&n;</pos>
+<gloss>rust</gloss>
+</sense>
+</entry>
+""".encode(
+    "utf-8"
+)
 
 entry_count = 0
 
@@ -14,4 +36,5 @@ with gzip.open(IN_FILE, "rb") as f, gzip.open(OUT_FILE, "wb") as g:
             entry_count += 1
             if entry_count == NUM_ENTRIES:
                 break
+    g.write(AKASABI_ENTRY)
     g.write(b"</JMdict>\n")
